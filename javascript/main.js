2df846e1f98b531c64911fe2aade8bfa200728e2
Yan Xu <xuyantc@msn.com>
2014-12-19 10:54:07 -0500

    countdown change to progress bar

--- javascript/main.js
+++ javascript/main.js
@@ -3,10 +3,17 @@
 	var $slider = document.getElementsByClassName("container")[0];
 	var timeout;
 
+	/*
 	$slider.addEventListener("mouseenter", function(){
 		timeout = setTimeout(function(){
 			document.getElementById("modal").className = 'modal show';
 		} ,3000);
+	});
+*/
+
+
+	$slider.addEventListener("click", function(){
+		document.getElementById("modal").className = 'modal show';
 	});
 
 	$slider.addEventListener("mouseleave", function(){
